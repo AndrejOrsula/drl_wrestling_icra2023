9558051a0da1883d27254eac4b9fc11370f3a00e
Andrej Orsula <orsula.andrej@gmail.com>
2023-05-10 22:41:29 +0200

    Add mirroring opponent for training [skip ci]
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- controllers/opponent/opponent.py
+++ controllers/opponent/opponent.py
@@ -1,33 +1,82 @@
-# Copyright 1996-2023 Cyberbotics Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import os
+import sys
 
-"""Minimalist controller example for the Robot Wrestling Tournament.
-   Demonstrates how to play a simple motion file."""
+MIRROR_PARTICIPANT: bool = False
 
-from controller import Robot, Motion
+if MIRROR_PARTICIPANT:
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+            "participant",
+        )
+    )
+    exec(
+        open(
+            os.path.join(
+                os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+                "participant",
+                "participant.py",
+            )
+        ).read()
+    )
+else:
+    from controller import Robot
 
+    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    from utils.image_processing import ImageProcessing as IP
+    from utils.fall_detection import FallDetection
+    from utils.gait_manager import GaitManager
+    from utils.camera import Camera
 
-class Wrestler (Robot):
-    def run(self):
-        motion = Motion('../motions/HandWave.motion')  # look into this text file, it's easy to understand
-        motion.setLoop(True)
-        motion.play()
-        time_step = int(self.getBasicTimeStep())  # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        while self.step(time_step) != -1:  # runs the hand wave motion in a loop until Webots quits
-            pass
+    class ScriptedController(Robot):
+        SMALLEST_TURNING_RADIUS = 0.1
+        SAFE_ZONE = 0.75
+        TIME_BEFORE_DIRECTION_CHANGE = 200
 
+        def __init__(self):
+            Robot.__init__(self)
+            self.time_step = int(self.getBasicTimeStep())
 
-# create the Robot instance and run main loop
-wrestler = Wrestler()
-wrestler.run()
+            self.camera = Camera(self)
+            self.fall_detector = FallDetection(self.time_step, self)
+            self.gait_manager = GaitManager(self, self.time_step)
+            self.heading_angle = 3.14 / 2
+            self.counter = 0
+
+        def run(self):
+            while self.step(self.time_step) != -1:
+                t = self.getTime()
+                self.gait_manager.update_theta()
+                if t < 4:
+                    self.start_sequence()
+                elif t > 4:
+                    self.fall_detector.check()
+                    self.walk()
+
+        def start_sequence(self):
+            self.gait_manager.command_to_motors(heading_angle=0)
+
+        def walk(self):
+            normalized_x = self._get_normalized_opponent_x()
+            desired_radius = (
+                (self.SMALLEST_TURNING_RADIUS / normalized_x)
+                if abs(normalized_x) > 1e-3
+                else None
+            )
+            if self.counter > self.TIME_BEFORE_DIRECTION_CHANGE:
+                self.heading_angle = -self.heading_angle
+                self.counter = 0
+            self.counter += 1
+            self.gait_manager.command_to_motors(
+                desired_radius=desired_radius, heading_angle=self.heading_angle
+            )
+
+        def _get_normalized_opponent_x(self):
+            img = self.camera.get_image()
+            _, _, horizontal_coordinate = IP.locate_opponent(img)
+            if horizontal_coordinate is None:
+                return 0
+            return horizontal_coordinate * 2 / img.shape[1] - 1
+
+    wrestler = ScriptedController()
+    wrestler.run()

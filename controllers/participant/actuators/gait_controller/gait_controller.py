974b784b9d74b2980f475375ab98f9730d355807
Andrej Orsula <orsula.andrej@gmail.com>
2023-05-11 22:36:28 +0200

    Copy over gait controller (slightly modified) [skip ci]
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ controllers/participant/actuators/gait_controller/gait_controller.py
@@ -0,0 +1,69 @@
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .ellipsoid_gait_generator import EllipsoidGaitGenerator
+from .kinematics import Kinematics
+
+
+class GaitController:
+    """Connects the Kinematics class and the EllipsoidGaitGenerator class together to have a simple gait interface."""
+
+    def __init__(self, robot, time_step, imu):
+        self.gait_generator = EllipsoidGaitGenerator(robot, time_step, imu=imu)
+        self.kinematics = Kinematics()
+        joints = [
+            "HipYawPitch",
+            "HipRoll",
+            "HipPitch",
+            "KneePitch",
+            "AnklePitch",
+            "AnkleRoll",
+        ]
+        self.L_leg_motors = [robot.getDevice(f"L{joint}") for joint in joints]
+        self.R_leg_motors = [robot.getDevice(f"R{joint}") for joint in joints]
+
+    def reset(self):
+        self.gait_generator.reset()
+        self.kinematics.reset()
+
+    def update_theta(self):
+        self.gait_generator.update_theta()
+
+    def set_step_amplitude(self, amplitude: float):
+        self.gait_generator.set_step_amplitude(amplitude)
+
+    def command_to_motors(self, desired_radius=0, heading_angle=0):
+        """
+        Compute the desired positions of the robot's legs for a desired radius (R > 0 is a right turn)
+        and a desired heading angle (in radians. 0 is straight on, > 0 is turning left).
+        Send the commands to the motors.
+        """
+        self.update_theta()
+        x, y, z, yaw = self.gait_generator.compute_leg_position(
+            is_left=False, desired_radius=desired_radius, heading_angle=heading_angle
+        )
+        right_target_commands = self.kinematics.inverse_leg(
+            x * 1e3, y * 1e3, z * 1e3, 0, 0, yaw, is_left=False
+        )
+        for command, motor in zip(right_target_commands, self.R_leg_motors):
+            motor.setPosition(command)
+
+        x, y, z, yaw = self.gait_generator.compute_leg_position(
+            is_left=True, desired_radius=desired_radius, heading_angle=heading_angle
+        )
+        left_target_commands = self.kinematics.inverse_leg(
+            x * 1e3, y * 1e3, z * 1e3, 0, 0, yaw, is_left=True
+        )
+        for command, motor in zip(left_target_commands, self.L_leg_motors):
+            motor.setPosition(command)
---
+++ controllers/participant/constants/kinematics.py
@@ -0,0 +1,27 @@
+"""
+Constants for the inverse kinematics code from this paper:
+N. Kofinas, “Forward and inverse kinematics for the NAO humanoid robot,” Diploma Thesis,
+Technical University of Crete, Greece, 2012,
+available at: https://www.cs.umd.edu/~nkofinas/Projects/KofinasThesis.pdf
+C++ code available at: https://github.com/kouretes/NAOKinematics
+/!\ This code works in millimeters, not meters like Webots
+"""
+
+# Constants from paper's C++ code
+ShoulderOffsetY = 98.0
+ElbowOffsetY = 15.0
+UpperArmLength = 105.0
+ShoulderOffsetZ = 100.0
+LowerArmLength = 57.75
+HandOffsetX = 55.95
+HandOffsetZ = 12.31
+HipOffsetZ = 85.0
+HipOffsetY = 50.0
+ThighLength = 100.0
+TibiaLength = 102.9
+FootHeight = 45.11
+NeckOffsetZ = 126.5
+CameraBottomX = 48.8
+CameraBottomZ = 23.81
+CameraTopX = 53.9
+CameraTopZ = 67.9

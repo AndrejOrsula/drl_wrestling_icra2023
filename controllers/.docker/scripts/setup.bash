f5a336dd7bdec3d09aceb293981fe23f5b857225
Andrej Orsula <orsula.andrej@gmail.com>
2023-05-14 01:02:07 +0200

    Update Dockerfile [skip ci]
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- controllers/.docker/scripts/setup.bash
+++ controllers/.docker/scripts/setup.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
-apt-get update && \
-    DEBIAN_FRONTEND=noninteractive \
-    apt-get install -yq --no-install-recommends git && \
-    rm -rf /var/lib/apt/lists/* && \
-    pip install --no-cache-dir "dreamerv3 @ git+https://github.com/danijar/dreamerv3.git" && \
-    pip install --no-cache-dir --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html && \
-    wget -q https://raw.githubusercontent.com/danijar/dreamerv3/main/dreamerv3/configs.yaml -O /usr/local/lib/python3.8/dist-packages/dreamerv3/configs.yaml
+## Install Python dependencies
+pip3 install -qqq --no-cache-dir ahrs opencv-python scipy
+pip3 install -qqq --no-cache-dir "dreamerv3 @ git+https://github.com/danijar/dreamerv3.git"
+pip3 install -qqq --no-cache-dir --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+wget -q https://raw.githubusercontent.com/danijar/dreamerv3/main/dreamerv3/configs.yaml -O "$(pip show dreamerv3 | grep Location: | cut -d' ' -f2)/dreamerv3/configs.yaml"
 
-pip install --no-cache-dir gdown && \
-    mkdir -p /usr/local/webots-project/controllers/participant/models && \
-    gdown -q 15gZJPC3yW-vfb8s4dJfxkPJR8U8NFEtA -O /usr/local/webots-project/controllers/participant/models/model01.ckpt
+## Download models
+pip3 install -qqq --no-cache-dir gdown
+mkdir -p /usr/local/webots-project/controllers/participant/models
+gdown -q 15gZJPC3yW-vfb8s4dJfxkPJR8U8NFEtA -O /usr/local/webots-project/controllers/participant/models/model01.ckpt

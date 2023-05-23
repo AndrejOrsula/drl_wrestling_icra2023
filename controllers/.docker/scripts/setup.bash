0a5fe2ee548b18376b937fd93b21857fdcf75d20
Andrej Orsula <orsula.andrej@gmail.com>
2023-05-23 02:37:41 +0200

    Install DreamerV3 from a stable fork [skip ci]
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- controllers/.docker/scripts/setup.bash
+++ controllers/.docker/scripts/setup.bash
@@ -3,6 +3,6 @@ set -e
 
 ## Install Python dependencies
 pip3 install -qqq --no-cache-dir ahrs opencv-python scipy
-pip3 install -qqq --no-cache-dir "dreamerv3 @ git+https://github.com/danijar/dreamerv3.git"
+pip3 install -qqq --no-cache-dir "dreamerv3 @ git+https://github.com/AndrejOrsula/dreamerv3.git"
 pip3 install -qqq --no-cache-dir --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 wget -q https://raw.githubusercontent.com/danijar/dreamerv3/main/dreamerv3/configs.yaml -O "$(pip show dreamerv3 | grep Location: | cut -d' ' -f2)/dreamerv3/configs.yaml"
